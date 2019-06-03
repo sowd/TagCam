3d979e124de5e8b066b3de6cc21c391b9aef54f1
Shigeru Owada <pub@toyhack.club>
2019-06-03 13:18:08 +0900

    Default settings changed

--- app/src/main/java/com/hoikutech/tagcam/preview/Preview.java
+++ app/src/main/java/com/hoikutech/tagcam/preview/Preview.java
@@ -2691,9 +2691,9 @@ public class Preview implements SurfaceHolder.Callback, TextureView.SurfaceTextu
                     // whilst devices with flash should support flash_auto, we'll also be in this codepath for front cameras with
                     // no flash, as instead the available options will be flash_off, flash_frontscreen_auto, flash_frontscreen_on
                     // see testTakePhotoFrontCameraScreenFlash
-                    if( supported_flash_values.contains("flash_auto") )
-                        updateFlash("flash_auto", true);
-                    else
+                    //if( supported_flash_values.contains("flash_auto") )
+                    //    updateFlash("flash_auto", true);
+                    //else
                         updateFlash("flash_off", true);
                 }
             }
